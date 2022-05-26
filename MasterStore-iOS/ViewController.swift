b24559d71654058d56358c0435adc7cfdb81289b
mrs1669 <mrs1669.develop@gmail.com>
2022-05-26 15:05:21 +0900

    initial commit

---
+++ MasterStore-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasterStore-iOS
+//
+//  Created by 村石拓海 on 2022/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
